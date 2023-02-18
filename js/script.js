e3e82bd2b015e36896b2e0cb5a3f933084951a0a
Stefan Bezzant <stefan.bezzant@gmail.com>
2023-02-18 21:52:08 +0000

    Add overlay to slider with logo, and down arrow to the next section

--- js/script.js
+++ js/script.js
@@ -6,4 +6,13 @@ $(document).ready(function(){
         mobileFirst: true,
         touchThreshold: 3,
     });
+
+    $('#introSlider').prepend("<div class='welcome-text-container'><p>Welcome to</p><img src='./img/hardin-beach-resort-text-logo.png'></div><div class='overlay'></div>")
+});
+
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
   });
