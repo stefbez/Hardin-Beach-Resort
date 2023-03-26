8fd96049b2bbd3a92fa71a435d38117371920982
Stefan Bezzant <stefan.bezzant@gmail.com>
2023-03-26 21:35:39 +0100

    Add more accomodation images ready for slider and clean up js file.

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ $(document).ready(function(){
     });
 
     $('#introSlider').prepend("<div class='welcome-text-container'><p>Welcome to</p><img src='./img/hardin-beach-resort-text-logo.png'></div><div class='overlay'></div>")
-});
 
-$(function() {
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    $(function() {
+        $('a[href*=#]').on('click', function(e) {
+            e.preventDefault();
+            $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+        });
     });
-  });
+});
