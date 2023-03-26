3f158ee335b1682129629b75bc3329c89f0b7ba8
Stefan Bezzant <stefan.bezzant@gmail.com>
2023-03-26 23:20:37 +0100

    Add to about section including a menu popup, functionality and styling to section

--- js/script.js
+++ js/script.js
@@ -15,4 +15,28 @@ $(document).ready(function(){
             $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
         });
     });
+
+    // Get the modal
+    var modal = document.getElementById("menuModal");
+    document.getElementById("menuLink").addEventListener('click', function(e) {
+        e.preventDefault();
+        console.log("CLICK")
+        modal.style.display = "flex";
+    })
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    // var img = document.getElementById("myImg");
+    // var modalImg = document.getElementById("img01");
+    // var captionText = document.getElementById("caption");
+    // .onclick = function(){
+    // modalImg.src = this.src;
+    // captionText.innerHTML = this.alt;
+    // }
+
+    // Get the <span> element that closes the modal
+    var closeMenu = document.getElementsByClassName("closeMenu")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    closeMenu.onclick = function() { 
+    modal.style.display = "none";
+    }
 });
